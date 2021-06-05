e90a2a6d7a4f21b35ef345a7910a1416836be89b
kerekmarci <kerekmarci@gmail.com>
2021-06-05 18:12:09 +0000

    Registration page and function added

--- app.py
+++ app.py
@@ -24,6 +24,34 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():    
+    if request.method == "POST":
+        # Check if username already exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "name": request.form.get("name"),
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password")),            
+            "profile_picture": request.form.get("profile_picture")
+        }
+
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie        
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return render_template("register.html")
+
+    return render_template("register.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
