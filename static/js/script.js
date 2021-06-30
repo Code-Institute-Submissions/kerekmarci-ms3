d57f5d6249d45c6172f8a4d7f7a85fe0557c49eb
kerekmarci <kerekmarci@gmail.com>
2021-06-30 10:57:19 +0000

    Form validation applied to Upload recipe page

---
+++ static/js/script.js
@@ -0,0 +1,42 @@
+let ingredientInputContainer = document.getElementById("ingredientInputContainer");
+let ingredientsInput = document.getElementsByName("ingredients");
+let methodInputContainer = document.getElementById("methodInputContainer");
+let methodInput = document.getElementsByName("method");
+firstIngredient = ingredientInputContainer.children[1];
+firstMethod = methodInputContainer.children[1];
+firstIngredient.addEventListener("input", enableNext);
+firstMethod.addEventListener("input", enableNext);
+
+function enableNext() {
+    this.nextElementSibling.disabled = false;
+}
+
+function addIngredientField() {
+    event.preventDefault();
+    let input = document.createElement("input");
+    input.type = "text";
+    input.className = "form-control mt-3";
+    input.name = "ingredients";
+    input.required = true;
+    ingredientInputContainer.appendChild(input);    
+}
+
+function removeIngredientField() {
+    event.preventDefault();
+    ingredientInputContainer.removeChild(ingredientInputContainer.lastChild);
+}
+
+function addMethodField() {
+    event.preventDefault();
+    let input = document.createElement("input");
+    input.type = "text";
+    input.className = "form-control mt-3";
+    input.name = "ingredients";
+    input.required = true;
+    methodInputContainer.appendChild(input);    
+}
+
+function removeMethodField() {
+    event.preventDefault();
+    methodInputContainer.removeChild(methodInputContainer.lastChild);
+}
